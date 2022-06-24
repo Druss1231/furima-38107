e94c14137d8d05b2b1cc35712f94047e8295c4b6
Druss1231 <drussyryr@gmail.com>
2022-06-24 17:53:37 +0900

    ユーザーモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
   validates :nickname, presence: true       
-  validates :firstname, presence: true               
-  validates :lastname, presence: true           
-  validates :firstname_kana, presence: true 
-  validates :lastname_kana, presence: true               
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}               
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/  }           
+  validates :firstname_kana, presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
+  validates :lastname_kana, presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}               
   validates :birthday, presence: true           
   
 end
