31d049e459245b2d9770e76b4ffb5c0f8afeb2cd
Druss1231 <drussyryr@gmail.com>
2022-06-24 16:31:29 +0900

    rspec導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+    end
+    it "emailが空では登録できない" do
+    end
+    it "firstnameが空では登録できない" do
+    end
+    it "lastnameが空では登録できない" do
+    end
+    it "firstname_kanaが空では登録できない" do
+    end
+    it "lastname_kanaが空では登録できない" do
+    end
+    it "birthdayが空では登録できない" do
+    end
+
+
+  end
+end
