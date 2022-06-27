70d04e34b05f4c1bfbb2a6aa0df070c13ee0a01d
Druss1231 <drussyryr@gmail.com>
2022-06-27 15:17:20 +0900

    itemモデルのvalidate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :title,       presence: true 
+  validates :price,       presence: true 
+  validates :fee,         presence: true 
+  validates :description, presence: true 
+  validates :category,    presence: true 
+  validates :condition,   presence: true 
+  validates :from,        presence: true 
+  validates :ship_date,   presence: true 
+
+  has_one_attached :image
 end
