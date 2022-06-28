fe8e6aa9f908a9f2cf9bde65a5fd2f09d64bff92
Druss1231 <drussyryr@gmail.com>
2022-06-28 18:15:21 +0900

    priceのvalidationに整数を追加した

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :title,        presence: true, length: { maximum: 40 }
-  validates :price,        numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,        numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :fee_id,       numericality: { other_than: 1, message: "can't be blank" }
   validates :description,  presence: true, length: { maximum: 1000 }
   validates :category_id,  numericality: { other_than: 1, message: "can't be blank" }
