bb2208f6991feb3210f9233bd8d10db7d73ec68f
Druss1231 <drussyryr@gmail.com>
2022-06-28 14:30:24 +0900

    varidationの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
-  validates :title,        presence: true 
-  validates :price,        numericality:{greater_than_or_equal_to: 300, :less_than_or_equal_to:9999999}
+  validates :title,        presence: true, length: { maximum: 40 }
+  validates :price,        numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :fee_id,       numericality: { other_than: 1 , message: "can't be blank"}
-  validates :description,  presence: true 
+  validates :description,  presence: true, length: { maximum: 1000 }
   validates :category_id,  numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :from_id,      numericality: { other_than: 1 , message: "can't be blank"} 
