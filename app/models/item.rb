a522899407aff7b81b2de815f0f7260eb0e2651e
Druss1231 <drussyryr@gmail.com>
2022-06-28 19:19:16 +0900

    商品一覧表示、soldoutの表示設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,9 @@ class Item < ApplicationRecord
   validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :image, presence:true
   has_one_attached :image
+
   belongs_to :user
+  has_one :history
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
