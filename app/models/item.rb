b21ccfc996d1bf1769e475b1c5f10b1d8d378447
Druss1231 <drussyryr@gmail.com>
2022-06-28 17:04:20 +0900

    itemテーブルにuserを紐付けた

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   validates :from_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
   has_one_attached :image
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
