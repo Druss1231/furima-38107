2e9d3cdbd98e54a1c06df7f1130ebd7e67a5254a
Druss1231 <drussyryr@gmail.com>
2022-06-28 15:00:30 +0900

    createアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
-  def create
+    def create
+      @item = Item.new(item_params) 
+      if @item.save
+       redirect_to root_path  
+       else
+         render :new
+       end
+      end
+
+      private
+      def item_params
+        params.require(:item).permit(:title, :price, :description, :image, :category_id, :condition_id, :fee_id, :from_id, :ship_date_id, )
+    end
   end
-  
-end
