32bd650c87127aaf5ed02397ffa053fd373adafe
Druss1231 <drussyryr@gmail.com>
2022-07-06 15:14:09 +0900

    不正なアクセスのリダイレクト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
     redirect_to root_path if @item.order.present? || (user_signed_in? && current_user.id == @item.user_id) || !user_signed_in?
