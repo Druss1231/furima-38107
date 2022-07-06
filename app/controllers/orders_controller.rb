cd62ee05142203fe50cb9da0c35605c36b1747bd
Druss1231 <drussyryr@gmail.com>
2022-07-06 18:43:13 +0900

    rubocopで修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,4 @@
 class OrdersController < ApplicationController
-
   before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
 
@@ -18,27 +17,28 @@ class OrdersController < ApplicationController
       @order_destination.save
       redirect_to root_path
     else
-      render :index 
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_destination).permit(:post_code, :from_id, :city, :address_line, :building, :phone).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_destination).permit(:post_code, :from_id, :city, :address_line, :building, :phone).merge(
+      token: params[:token], item_id: params[:item_id], user_id: current_user.id
+    )
   end
 
-   def pay_item
-     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
-     Payjp::Charge.create(
-       amount: @item.price,  # 商品の値段
-       card: order_params[:token],    # カードトークン
-       currency: 'jpy'                 # 通貨の種類（日本円）
-     )
-   end
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 
-   def set_item
+  def set_item
     @item = Item.find(params[:item_id])
-   end
-
+  end
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -5,17 +5,18 @@ class OrderDestination
   with_options presence: true do
     validates :token
     validates :user_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true}
-    validates :from_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)', allow_blank: true }
+    validates :from_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address_line
-    validates :phone, format: {with: /\A\d{10}$|^\d{11}\z/, allow_blank: true}
+    validates :phone, format: { with: /\A\d{10}$|^\d{11}\z/, allow_blank: true }
     validates :item_id
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    destination = Destination.new(post_code: post_code, prefecture: from_id, city: city, address_line: address_line, building: building, order_id: order.id)
+    destination = Destination.new(post_code: post_code, prefecture: from_id, city: city, address_line: address_line,
+                                  building: building, order_id: order.id)
     destination.save!
   end
 end
