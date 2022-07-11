863e937882ff8b319cd15379a7dd3fc0b64abd9f
Druss1231 <drussyryr@gmail.com>
2022-07-11 16:36:15 +0900

    Update 20220623071020_create_items.rb

--- db/migrate/20220623071020_create_items.rb
+++ db/migrate/20220623071020_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :title,       null: false
-      t.text    :price,       null: false
-      t.text    :fee,         null: false
-      t.text    :description, null: false
-      t.integer :category, null: false
-      t.text    :condition_id, null: false
-      t.text    :from_id,     null: false
-      t.text    :shipdate_id, null: false
+      t.string :title
+      t.text :price
+      t.text :fee
+      t.text :description
+      t.text :category
+      t.text :condition
+      t.text :from
+      t.text :ship_date
 
       t.timestamps
     end
