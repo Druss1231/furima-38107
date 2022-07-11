a8de879930d47059523f14554028cba3a39726f3
Druss1231 <drussyryr@gmail.com>
2022-07-11 16:32:20 +0900

    Update 20220623071020_create_items.rb

--- db/migrate/20220623071020_create_items.rb
+++ db/migrate/20220623071020_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text    :price,       null: false
       t.text    :fee,         null: false
       t.text    :description, null: false
-      t.integer :category_id, null: false
+      t.integer :category, null: false
       t.text    :condition_id, null: false
       t.text    :from_id,     null: false
       t.text    :shipdate_id, null: false
